a04c6285ea8b885558e6471f39a7968744c36f2a
Droidca <95036101+Droidca@users.noreply.github.com>
2022-01-10 17:46:14 +0000

    Create toDoList functions
    Add/Cross/Delete

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function newItem () {
+
+  //To add items to the list
+
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  let list = $('#list');
+
+  list.append(li);
+
+  if(inputValue === '') {
+    alert('Please fill the textbox!');
+    li.remove(inputValue);
+  } else {
+      li.append(inputValue);
+    }
+
+  // To cross items from the list
+
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', crossOut);
+
+  // To add close Button
+
+  let button = $('<crossOutButton>X</crossOutButton>');
+  li.append(button);
+
+  // To delete list item
+
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+
+  button.on('click', deleteListItem);
+}
